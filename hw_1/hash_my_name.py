50a5569bd2efad1dfc52cb10d8dd145b1ceeaf42
filipovsky <filipovskii.off@gmail.com>
2014-02-28 18:33:49 +0400

    File name variable.

--- hw_1/hash_my_name.py
+++ hw_1/hash_my_name.py
@@ -2,9 +2,10 @@ import sha
 
 name = raw_input("What's your first name? > ")
 name = name.lower()
+file_name = '{}.txt'.format(name)
 
-with open(name + '.txt', 'w') as f:
+with open(file_name, 'w') as f:
     name_hash = sha.new(name).hexdigest()
     f.write(name_hash)
     
-print('Hash saved to {}.txt'.format(name))
+print('Hash saved to {}.'.format(file_name))
