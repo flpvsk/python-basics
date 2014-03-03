faa8adadc286361965634007d822dcfb52bfb41f
perelesok <gafez@mail.ru>
2014-03-03 10:58:01 +0400

    Simeon.py is printing my(your) name

---
+++ practice_1/simeon.py
@@ -0,0 +1,11 @@
+import sha
+
+name = raw_input("What's your first name? > ")
+name = name.lower()
+file_name = '{}.txt'.format(name)
+
+with open(file_name, 'w') as f:
+ name_hash = sha.new(name).hexdigest()
+ f.write(name_hash)
+
+print('Hash saved to {}.'.format(file_name))
