7d1f660b20594e77ccfb4b73fcd2e3d8d42540c9
filipovsky <filipovskii.off@gmail.com>
2014-03-05 22:19:35 +0400

    Unpacking iterable.

--- examples_2/09_iterables.py
+++ examples_2/09_iterables.py
@@ -23,3 +23,13 @@ print("Dict:")
 for key in {"key1": 1, 2: 2}:
     print("  %r" % key)
 print_stars()
+
+# Iterables unpacking
+print("Unpack")
+x, y = [1, 2]
+print("x, y = [1, 2]       -> x = %r\ty = %r" % (x, y))
+x, y = 'xy'
+print("x,y = 'xy'          -> x = %r\ty = %r" % (x, y))
+x, y = ('v1', 'v2')
+print("x, y = ('v1', 'v2') -> x = %r\ty = %r" % (x, y))
+print_stars()
