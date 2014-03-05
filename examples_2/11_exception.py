a24f9ce4cc829065362707fd9cb46b4588451e63
filipovsky <filipovskii.off@gmail.com>
2014-03-05 19:06:20 +0400

    Exceptions.

---
+++ examples_2/11_exception.py
@@ -0,0 +1,40 @@
+from utils import print_stars
+
+
+# Basic try - except - [else] - finally
+# `else` - optional
+# should have `except` or `finally` or both
+print("assert False")
+try:
+    assert False
+    print("Execution continued")
+except AssertionError as e:
+    print("Exception caught")
+else:
+    print("No exception thrown")
+finally:
+    print("Finally block")
+print_stars()
+
+
+print("assert True")
+try:
+    assert True
+    print("Execution continued")
+except AssertionError as e:
+    print("Exception caught")
+else:
+    print("No exception thrown")
+finally:
+    print("Finally block")
+print_stars()
+
+
+def raise_assertion_error(msg):
+    raise AssertionError(msg)
+
+raise_assertion_error("Custom Message")
+
+# ValueError ~ IllegalStateException, IllegalArgumentException in Java
+# TypeError - object of incorrect type was used
+# AssertionError ~ AssertionError in Java
