c9ff5acf3399dda9ebc121948490423243593529
yakovkoshkin <yakov.koshkin@gmail.com>
2014-03-09 00:54:27 +0400

    to_bool

--- practice_2/yakov_iter.py
+++ practice_2/yakov_iter.py
@@ -8,6 +8,7 @@ Created on 08 March 2014
 def dividers(x):
     res = []
     try:
+        x = abs(x)
         for i in xrange(1, x / 2 + 1):
             if x % i == 0:
                 res.append(i)
@@ -15,14 +16,22 @@ def dividers(x):
         print e.message
     return res
 
-
+print "dividers"
 print dividers(147)
 print dividers("s")
+print dividers(-15)
 
 
 def to_bool(lst):
-    res = [bool(item) for item in lst]
+    res = []
+    try:
+        res = [bool(item) for item in lst]
+    except Exception as e:
+        print e.message
     return res
 
+print "to_bool"
 print to_bool([])
 print to_bool([1, 0, [], ()])
+print to_bool(2)
+print to_bool((2, 4, 0))
