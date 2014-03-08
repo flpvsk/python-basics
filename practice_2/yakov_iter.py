6eb7e93329d2af21304f27666606a2b051627b6e
yakovkoshkin <yakov.koshkin@gmail.com>
2014-03-08 19:35:28 +0400

    Dividers: Try/except section for Dividers(), to_bool empty func
    Signed-off-by: yakovkoshkin <yakov.koshkin@gmail.com>

--- practice_2/yakov_iter.py
+++ practice_2/yakov_iter.py
@@ -7,9 +7,17 @@ Created on 08 March 2014
 
 def dividers(x):
     res = []
-    for i in xrange(1, x / 2 + 1):
-        if x % i == 0:
-            res.append(i)
+    try:
+        for i in xrange(1, x / 2 + 1):
+            if x % i == 0:
+                res.append(i)
+    except Exception as e:
+        print e.message
     return res
 
 print dividers(147)
+print dividers("s")
+
+
+def to_bool(lst):
+    pass
