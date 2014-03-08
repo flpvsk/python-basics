2056f667f1cc74c03fc9712097ac5699a250ea7b
yakovkoshkin <yakov.koshkin@gmail.com>
2014-03-08 19:25:58 +0400

    Dividers: function and small test

--- practice_2/yakov_iter.py
+++ practice_2/yakov_iter.py
@@ -6,4 +6,10 @@ Created on 08 March 2014
 
 
 def dividers(x):
-    pass
+    res = []
+    for i in xrange(1, x / 2 + 1):
+        if x % i == 0:
+            res.append(i)
+    return res
+
+print dividers(147)
