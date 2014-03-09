ebe0e67565a5506cdc3d40937400d5adb6150979
yakovkoshkin <yakov.koshkin@gmail.com>
2014-03-10 00:04:57 +0400

    Now tests print values and result

--- hw_2/yakov_iter.py
+++ hw_2/yakov_iter.py
@@ -54,8 +54,20 @@ wrd = "words"
 lns = "lines"
 
 
-def wc(s):
-    res = {sym: 0, wrd: 0, lns: 0}
+def wc(st):
+    s = 0
+    w = 0
+    l = 0
+    res = {sym: s, wrd: w, lns: l}
+    if isinstance(st, str):
+        try:
+            # Find symbols number
+            s = len(st)
+            # Find words number
+            # Find lines number
+        except:
+            pass
+        res = {sym: s, wrd: w, lns: l}
     return res
 
 
@@ -64,10 +76,10 @@ def test_wc():
     exp1 = {sym: 10, wrd: 1, lns: 1}
     exp2 = {sym: 0, wrd: 0, lns: 0}
     exp3 = {sym: 5, wrd: 3, lns: 3}
-    ya.test(wc("abcdefghij"), exp1)
-    ya.test(wc(""), exp2)
-    ya.test(wc("a\nb\nc"), exp3)
-    ya.test(wc(1), exp2)
-    ya.test(wc(["a", "bd"]), exp2)
+    print ya.assert_equal(wc("abcdefghij"), exp1)
+    print ya.assert_equal(wc(""), exp2)
+    print ya.assert_equal(wc("a\nb\nc"), exp3)
+    print ya.assert_equal(wc(1), exp2)
+    print ya.assert_equal(wc(["a", "bd"]), exp2)
 
 test_wc()
