92251b6813a68211d141cf9ed5ca2f96497ceeef
yakovkoshkin <yakov.koshkin@gmail.com>
2014-03-09 13:01:02 +0400

    hw2: divide with tests created

--- hw_2/yakov_iter.py
+++ hw_2/yakov_iter.py
@@ -25,3 +25,26 @@ def test_unique():
     ya.test(unique([3, 2, 1]), [3, 2, 1])
 
 #test_unique()
+
+
+def divide(a, b):
+    res = [False, None]
+    try:
+        r = a / b
+    except:
+        return res
+    else:
+        return [True, r]
+
+
+def test_divide():
+    ya.divider("divide")
+    fls = [False, None]
+    ya.test(divide(10, 0), fls)
+    ya.test(divide(0, 10), [True, 0])
+    ya.test(divide("abc", "fgh"), fls)
+    ya.test(divide("100", "50"), fls)
+    ya.test(divide(-5.0, -2.0), [True, 2.5])
+    ya.test(divide([2], [1]), fls)
+
+#test_divide()
