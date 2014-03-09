e4cf4861db5f61ee6a6d597ab2df1761e55ec240
yakovkoshkin <yakov.koshkin@gmail.com>
2014-03-09 11:33:17 +0400

    unique

--- hw_2/yakov_iter.py
+++ hw_2/yakov_iter.py
@@ -3,11 +3,21 @@ Created on 09 March 2014
 
 @author: Yaha
 '''
+import practice_2.yakov_assertions as ya
 
 
 def unique(lst):
-    pass
+    res = []
+    i = 0
+    for item in lst:
+        if lst.index(item) == i:
+            res.append(item)
+        i = i + 1
+    return res
 
 
 def test_unique():
-    pass
+    ya.divider("unique")
+    ya.test(unique([1, 2, 3, 2, 1]), [1, 2, 3])
+
+test_unique()
