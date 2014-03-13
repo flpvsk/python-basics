71d273d73de13105914ab3d5ed867a828f81ad17
yakovkoshkin <yakov.koshkin@gmail.com>
2014-03-13 11:57:04 +0400

    Add empty test_runner

---
+++ practice_3/test_runner.py
@@ -0,0 +1,37 @@
+'''
+Created on Mar 13, 2014
+
+@author: Java Student
+'''
+
+# key: test
+# value: None, 0-passed, 1-failed
+tests = dict()
+
+
+def add_test(fn):
+    pass
+
+
+def pending_tests():
+    pass
+
+
+def run():
+    pass
+
+
+def ran_tests():
+    pass
+
+
+def passed_tests():
+    pass
+
+
+def failed_tests():
+    pass
+
+
+def clear_state():
+    pass
