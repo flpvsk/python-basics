e784bd8f67f071dbb8bc287af7f00f3efdae4432
Maria Popova <maria.popova@db.com>
2014-03-20 18:57:46 +0400

    Practice 5, test cases classes

--- maria/autotest/TestRunnerLauncher.py
+++ maria/autotest/TestRunnerLauncher.py
@@ -1,46 +1,87 @@
+''' Launches TestRunner
+
+Extracts all tests from specified source and launch testRunner for them
+'''
+
 import importlib
+import sys
 from TestRunner import TestRunner
 
+
 class TestRunnerLauncher(object):
-    
     TEST_METHOD_PREFIX = "test_"
     SET_UP_METHOD_NAME = "set_up"
     TEAR_DOWN_METHOD_NAME = "tear_down"
-    
-    test_extractors_dict = {"module_functions" : __extract_tests_from_module_functions, 
-                            "class": __extract_tests_from_class}    
-    
+    TEST_FUNCTIONS_CONTAINER_TYPE_NAME = "module_functions"
+    CLASSES_TEST_CONTAINER_TYPE_NAME = "class"
+
     def __init__(self, test_module_name, test_container_type):
+        '''Specifies test runner launches
+
+        :param test_module_name: name of module with tests
+        :param test_container_type: test container type (class or functions)
+        '''
         self.test_module = importlib.import_module(test_module_name)
         self.test_container_type = test_container_type
-        
+        self.test_extractors_dict = \
+                {self.TEST_FUNCTIONS_CONTAINER_TYPE_NAME:
+                 self.__extract_tests_from_module_functions,
+                 self.CLASSES_TEST_CONTAINER_TYPE_NAME:
+                 self.__extract_tests_from_classes}
+
     def execute_tests(self):
-        test_containers = [f for f in dir(test_module) if f in test_module.__all__]
-        testRunner = TestRunner()
-        for func in test_functions:
+        '''Runs test runner for specified tests'''
+        all_tests = self.test_extractors_dict[self.test_container_type]()
+        for class_tests in all_tests:
+            testRunner = TestRunner()
             testRunner.clear_state()
-            testRunner.add_test(getattr(module, func))
+            testRunner.set_tests_set_up(class_tests[1])
+            testRunner.set_tests_tear_down(class_tests[2])
+            [testRunner.add_test(class_test) for class_test in class_tests[0]]
             testRunner.run()
             if len(testRunner.failed_tests()) != 0:
                 sys.exit(1)
             else:
                 sys.exit(0)
-            print test_classes
-            
-    def __extract_tests_from_module_functions(self):
-        return [getattr(self.test_module, f) for f in dir(self.test_module) if f in self.test_module.__all__]
-        
-    def __extract_tests_from_class(self):
-        return [getattr(self.test_module, f)() for f in dir(self.test_module) if self.__method_is_test(f)]
-    
-    def __method_is_test(self, method_name):
-        if method_name in self.test_module.__all__  and \
-            (method_name.startswith(self.TEST_METHOD_PREFIX) or method_name == self.SET_UP_METHOD_NAME or method_name == self.TEAR_DOWN_METHOD_NAME):
-            return True
-        return False
-    
-            
 
-  
-test_runner_launcher = TestRunnerLauncher("examples_5.todo_tests")  
-test_runner_launcher.execute_tests()
+    def __extract_tests_from_module_functions(self):
+        '''Returns tuple with tests.
+
+        Tuple consists of 3 pointers:
+        to list of test functions, to setup function and to teardown function.
+        As there is no setup and teardown functions for module test functions,
+        None is returned
+        '''
+        return ([getattr(self.test_module, f) for f in dir(self.test_module)
+                 if f in self.test_module.__all__], None, None)
+
+    def __extract_tests_from_classes(self):
+        '''Returns list of tuples with tests from classes.
+
+        Each tuple consists of 3 pointers:
+        to list of class test functions, to setup function and to
+        teardown function.
+        '''
+        test_classes = [getattr(self.test_module, f)() for f
+                         in dir(self.test_module)
+                         if f in self.test_module.__all__]
+        return [self.__get_tests(test_class) for test_class in test_classes]
+
+    def __get_tests(self, test_class):
+        '''Returns tuple with tests from class.
+
+        Tuple consists of 3 pointers:
+        to list of class test functions, to setup function and to
+        teardown function.
+        '''
+        set_up_method = None
+        tear_down_method = None
+        tests = []
+        for method_name in dir(test_class):
+            if method_name == self.SET_UP_METHOD_NAME:
+                set_up_method = getattr(test_class, method_name)
+            if method_name == self.TEAR_DOWN_METHOD_NAME:
+                tear_down_method = getattr(test_class, method_name)
+            if method_name.startswith(self.TEST_METHOD_PREFIX):
+                tests.append(getattr(test_class, method_name))
+        return (tests, set_up_method, tear_down_method)
--- maria/autotest/__main__.py
+++ maria/autotest/__main__.py
@@ -1,17 +1,8 @@
 import sys
-import importlib
-from TestRunner import TestRunner
+from TestRunnerLauncher import TestRunnerLauncher
 
 if __name__ == '__main__':
-    package_name = sys.argv[1]
-    module = importlib.import_module(package_name)
-    test_functions = [f for f in dir(module) if f in module.__all__]
-    testRunner = TestRunner()
-    for func in test_functions:
-        testRunner.clear_state()
-        testRunner.add_test(getattr(module, func))
-        testRunner.run()
-        if len(testRunner.failed_tests()) != 0:
-            sys.exit(1)
-        else:
-            sys.exit(0)
+    module_name = sys.argv[1]
+    test_container_type = sys.argv[2]
+    test_runner_launcher = TestRunnerLauncher(module_name, test_container_type)
+    test_runner_launcher.execute_tests()
