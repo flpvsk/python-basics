0a3b4dd2ed87f2758fceb0798885c9d93c292883
yakovkoshkin <yakov.koshkin@gmail.com>
2014-03-24 10:07:40 +0400

    Outputs failed tests with arguments

--- examples_4/yakov_todo_tests.py
+++ examples_4/yakov_todo_tests.py
@@ -10,31 +10,40 @@ import yakov.autotest.assertions as ass
 
 tst_pending = 'pending'
 tet_compl = 'completed'
-msg = ' doesn\'t work'
+
+
+def test_pass(fn, *args):
+    try:
+        fn(*args)
+    except:
+        print "{0}{1} failed".format(args[-1], args[:-1])
+        # I don't like this 'raise' but couldn't choose better approach
+        # And it works
+        raise
+    else:
+        #print "{0}{1} passed".format(fn.__name__, args)
+        pass
 
 
 def test_add():
     todo.add('bbb')
-    ass.assert_equal(todo.items(), (('bbb', tst_pending),), test_add.__name__ + msg)
+    test_pass(ass.assert_equal, todo.items(), (('bbb', tst_pending),), test_add.__name__)
 
 
 def test_clear():
     todo.clear()
-    try:
-        ass.assert_equal(todo.items(), (), test_clear.__name__ + msg)
-    except Exception as e:
-        print e.message
+    test_pass(ass.assert_equal, todo.items(), (0), test_clear.__name__)
 
 
 def test_new_add_index():
     todo.clear()
-    ass.assert_equal(todo.add('txt'), 0, test_new_add_index.__name__ + msg)
+    test_pass(ass.assert_equal, todo.add('txt'), 0, test_new_add_index.__name__)
 
 
 def test_existing_add_index():
     todo.clear()
     todo.add('zero')
-    ass.assert_equal(todo.add('one'), 1, test_existing_add_index.__name__ + msg)
+    test_pass(ass.assert_equal, todo.add('one'), 2, test_existing_add_index.__name__)
 
 
 def test_empty_add_error():
