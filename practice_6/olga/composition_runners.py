349e760f09868338efefb62df5db44a9c20c8dcc
mironovaOlga <olga.mironova@db.com>
2014-03-24 11:37:33 +0400

    practice 6_2

--- practice_6/olga/composition_runners.py
+++ practice_6/olga/composition_runners.py
@@ -1,10 +1,16 @@
 import traceback
 from assertions import *
-class TestRunnerBase(object):
+
+
+class TestRunnerReporter(object):
     tests = {}
     PENDING = "pending"
     FAILED = "failed"
     PASSED = "passed"
+    reporter = None
+    
+    def __init__(self, some_reporter):
+        self.reporter = some_reporter
     
     def add_test(self, fn):
         self.tests[fn.__name__] = ["pending", fn]
@@ -15,16 +21,16 @@ class TestRunnerBase(object):
     def run(self):
         for x in self.tests:
             if self.tests[x][0] == "pending":
-                self.report_test_started(x)
+                self.reporter.report_test_started(x)
                 try:
                     test = self.tests[x][1]
                     test()
                     self.tests[x] = ["passed", test]
-                    self.report_test_finished(x)
-                except Exception as e:
+                    self.reporter.report_test_finished(x)
+                except Exception:
                     self.tests[x] = ["failed", test]
-                    self.report_test_finished(x, traceback.format_exc())
-        print(self.report_all_finished())
+                    self.reporter.report_test_finished(x, traceback.format_exc())
+        self.reporter.report_all_finished(self.passed_tests(), self.failed_tests(), self.ran_tests())
 
 
     def ran_tests(self):
@@ -39,14 +45,9 @@ class TestRunnerBase(object):
     def clear_state(self):
         self.tests = {}
     
-    def report_all_finished(self):
-        return {self.PASSED: self.passed_tests(),
-                self.FAILED: self.failed_tests(),
-                "RAN": self.ran_tests()}
 
 
-
-class TestRunnerVerboseReporting(TestRunnerBase):
+class VerboseReporter(object):
     def report_test_started(self, test):
         print("Started test '{0}'".format(test))
     
@@ -56,9 +57,14 @@ class TestRunnerVerboseReporting(TestRunnerBase):
         else:
             print("Test '{0}' passed".format(test))
     
+    def report_all_finished(self, passed, failed, ran):
+        print({"PASSED": passed,
+                "FAILED": failed,
+                "RAN": ran})
+    
 
 
-class TestRunnerFailReporting(TestRunnerBase):
+class FailReporter(object):
     def report_test_started(self, x=None):
         pass
     
@@ -67,17 +73,24 @@ class TestRunnerFailReporting(TestRunnerBase):
             print("test '{0}' failed: {1}".format(test, e))
         else:
             print(".")
-    
+    def report_all_finished(self, passed, failed, ran):
+        print({"PASSED": passed,
+                "FAILED": failed,
+                "RAN": ran})
+
+
 
 if __name__ == "__main__":
-    my_runner = TestRunnerFailReporting()
+    my_runner = TestRunnerReporter(VerboseReporter())
     my_runner.add_test(test_assert_equal)
     my_runner.add_test(test_assert_is_none)
     my_runner.add_test(test_assert_not_equal)    
     my_runner.run()
     
-    my_runner2 = TestRunnerVerboseReporting()
-    my_runner2.add_test(test_assert_equal)
-    my_runner2.add_test(test_assert_is_none)
-    my_runner2.add_test(test_assert_not_equal)    
-    my_runner2.run()
+    
+    my_runner1 = TestRunnerReporter(FailReporter())
+    my_runner1.add_test(test_assert_equal)
+    my_runner1.add_test(test_assert_is_none)
+    my_runner1.add_test(test_assert_not_equal)    
+    my_runner1.run()
+    
