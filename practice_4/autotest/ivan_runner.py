18b10562889f8e21e84dca954c398adbff2fd636
paraiva <paraiva@RUMOSWS200243.ru.db.com>
2014-03-26 10:51:48 +0400

    conflicts were solved

--- practice_4/autotest/ivan_runner.py
+++ practice_4/autotest/ivan_runner.py
@@ -61,56 +61,6 @@ def foo():
 add_test(foo)
 print run(add_test(foo))
 
-
-def add_test(fn):
-    pending_test_list.append(fn)
-
-
-def pending_test():
-    for i in pending_test_list:
-        return i
-
-
-def run():
-    for i in pending_test_list:
-        try:
-            i()
-        except AssertionError as er:
-            print("An error occurred: " + er)
-            failed_test_list.append(i)
-        finally:
-            run_test_list.append(i)
-            #pending_test_list.remove(i)
-    return (len(run_test_list),
-            len(passed_test_list),
-            len(failed_test_list))
-
-
-def run_tests():
-        return (run_test_list)
-
-
-def passed_tests():
-    for i in passed_test_list:
-        return i
-
-
-def failed_tests():
-    for i in failed_test_list:
-        return i
-
-
-def clear_state():
-    global pending_test_list
-    global failed_test_list
-    global run_test_list
-    global passed_test_list
-
-    del pending_test_list
-    del failed_test_list
-    del run_test_list
-    del passed_test_list
-
 if __name__ == '__main__':
     add_test(test_that_fails)
     add_test(test_function_attribute_name)
