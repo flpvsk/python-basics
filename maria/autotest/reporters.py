526a8d4bdb9faa259f6ac2d99995a9f2aacef51c
Maria Popova <maria.popova@db.com>
2014-04-02 13:29:50 +0400

    Refactoring

--- maria/autotest/reporters.py
+++ maria/autotest/reporters.py
@@ -25,7 +25,7 @@ class FailReporter(object):
 class VerboseReporter(object):
 
     def report_test_started(self, test):
-        print "Test '{}' has been started".format(test.__name__)
+        print "Test '{}' has been started".format(get_full_name(test))
 
     def report_test_finished(self, test_result):
         print test_result
--- maria/autotest/test_names.py
+++ maria/autotest/test_names.py
@@ -1,24 +1,22 @@
 import re
 import os
-import sys
 from os import listdir
-from sets import Set
 
 
 __all__ = ('test_result_parser',)
 
 
 def test_result_parser(directory):
-    tests = Set([])
+    tests = set()
     for test_result_file in listdir(directory):
-        tests.update(extract_tests_from_file(os.path.join
-                                          (directory, test_result_file)))
+        path_to_report_file = os.path.join(directory, test_result_file)
+        tests.update(extract_tests_from_file(path_to_report_file))
     return tests
 
 
 def extract_tests_from_file(test_file):
     test_name_regexp = "Test '(?P<test_name>.*)' has been started"
-    tests = Set([])
+    tests = set()
     with open(test_file, 'r') as text_file:
         for line in text_file:
             match = re.match(test_name_regexp, line, re.I)
@@ -29,6 +27,6 @@ def extract_tests_from_file(test_file):
 
 
 if __name__ == '__main__':
-    test_directory = "C:\\test-results"
+    test_directory = "C:\\CVS\\test-results"
     for test_names in test_result_parser(test_directory):
-        sys.stdout.write(test_names + '\n')
+        print test_names
