6f3cfd45a6a871cb2c4183bde63e41bda631f6ef
arakann <nanuli.arakelyan@gmail.com>
2014-04-02 23:54:51 +0400

    old runner and reporter modules are moved to 'old'

--- anya/autotest/TestResult.py
+++
@@ -1,9 +0,0 @@
-class TestResult():
-    def __init__(self, test, test_result, stacktrace = ''):
-        if test.im_class == 'TestCase':
-            self.test_name = test.__module__ + '.' + test.im_class.__name__ + '.' + test.__name__
-        else:
-            self.test_name = test.__module__ + '.'+ test.__name__
-        self.test_name = test.__module__ +'.' + test.__name__
-        self.test_result = test_result
-        self.stack_trace = stacktrace
