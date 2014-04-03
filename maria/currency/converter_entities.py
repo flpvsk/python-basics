f5a68b58b6f8336961683ed207149351373b03b6
Maria Popova <maria.popova@db.com>
2014-04-03 13:36:56 +0400

    HomeWork 8: currency converter

--- maria/currency/converter_entities.py
+++ maria/currency/converter_entities.py
@@ -24,13 +24,29 @@ class Currency(object):
 class Money(object):
 
     def __init__(self, amount, currency):
-        self.amount = amount
-        self.currency = currency
+        self._amount = amount
+        self._currency = currency
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @property
+    def currency(self):
+        return self._currency
 
     def convert_to(self, target_currency):
         return Money(self.amount *
                      self.currency.exchange_rates[target_currency.name],
                      target_currency)
+
+    def __add__(self, other):
+        total_amount = self.amount + other.convert_to(self.currency).amount
+        return Money(total_amount, self.currency)
+
+    def __sub__(self, other):
+        total_amount = self.amount - other.convert_to(self.currency).amount
+        return Money(total_amount, self.currency)
 
     def __str__(self):
         return str(self.amount) + self.currency.symbol
--- maria/currency/main_products.py
+++ maria/currency/main_products.py
@@ -4,9 +4,10 @@ from converter_entities import Money
 __all__ = ('RUBLE', 'DOLLAR', 'EURO', 'rubles', 'dollars', 'euro')
 
 
-RUBLE = Currency("Rubles", "rub", {'Dollar': 0.0277, 'Euro': 0.208})
-DOLLAR = Currency("Dollar", "$", {'Rubles': 36, 'Euro': 0.750})
-EURO = Currency("Euro", "eur", {'Rubles': 48, 'Dollar': 1.333})
+RUBLE = Currency("Rubles", "rub",
+                 {'Rubles': 1, 'Dollar': 0.0277, 'Euro': 0.208})
+DOLLAR = Currency("Dollar", "$", {'Dollar': 1, 'Rubles': 36, 'Euro': 0.750})
+EURO = Currency("Euro", "eur", {'Euro': 1, 'Rubles': 48, 'Dollar': 1.333})
 
 
 def rubles(n):
@@ -19,6 +20,3 @@ def dollars(n):
 
 def euro(n):
     return Money(n, EURO)
-
-
-print dollars(10).convert_to(EURO)
