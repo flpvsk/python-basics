eb93021a1e781f30561fbe6f0de52081cbcf1528
filipovskii <filipovskii.off@gmail.com>
2014-04-03 09:25:34 +0400

    Quick fix win sockets.

--- examples_9/01_socket_server.py
+++ examples_9/01_socket_server.py
@@ -2,7 +2,7 @@ from socket import socket
 
 def main():
     server_sock = socket()
-    server_sock.bind(('', 8000))
+    server_sock.bind(('', 8001))
     server_sock.listen(1)
 
     try:
@@ -10,14 +10,18 @@ def main():
         print('Client connected from {}'.format(addr))
 
         try:
-
+            data = ''
             while True:
-                data = conn.recv(1024)
+                data += conn.recv(1024)
                 if not data:
                     break
 
+                if '\n' not in data:
+                    continue
+
                 print('Got data from {}'.format(addr))
                 conn.send('Got: {}'.format(data))
+                data = ''
 
         finally:
             print('Connection from {} closed'.format(addr))
