ec6cfb4e3a9f69c733690c640ad761d550f4ffc1
Irina Kraeva <irinakraeva@inbox.ru>
2014-04-03 11:58:17 +0400

    assertions changes

--- ivan/autotest/assertions.py
+++ ivan/autotest/assertions.py
@@ -9,70 +9,40 @@ __all__ = ('assert_equal', 'assert_not_equal', 'assert_true', 'assert_false',
 
 
 def assert_equal(a, b, message='not equal'):
-    try:
         assert a == b, message
-    except:
-        AssertionError(message)
 
 
 def assert_not_equal(a, b, message='equal'):
-    try:
         assert a != b, message
-    except:
-        AssertionError(message)
 
 
 def assert_true(x, message='false'):
-    try:
         assert x, message
-    except:
-        AssertionError(message)
 
 
 def assert_false(x, message='true'):
-    try:
         assert not x, message
-    except:
-        AssertionError(message)
 
 
 def assert_is(a, b, message=' is not'):
-    try:
         assert a is b, message
-    except:
-        AssertionError(message)
 
 
 def assert_is_not(a, b, message='is'):
-    try:
         assert a is not b, message
-    except:
-        AssertionError(message)
 
 
 def assert_is_none(x, message='not None'):
-    try:
         assert x is None, message
-    except:
-        AssertionError(message)
 
 
 def assert_is_not_none(x, message='None'):
-    try:
         assert x is not None, message
-    except:
-        AssertionError(message)
 
 
 def assert_in(a, b, message='not in'):
-    try:
         assert a in b, message
-    except:
-        AssertionError(message)
 
 
 def assert_not_in(a, b, message='in'):
-    try:
         assert a not in b, message
-    except:
-        AssertionError(message)
--- practice_2/ivan_assertions.py
+++ practice_2/ivan_assertions.py
@@ -6,10 +6,7 @@ Created on 20 Mar 2014
 
 
 def assert_equal(a, b, message='not equal'):
-    try:
         assert a == b, message
-    except:
-        AssertionError(message)
 
 
 #print 'assert_equal function tests: \n'
@@ -18,30 +15,21 @@ def assert_equal(a, b, message='not equal'):
 
 
 def assert_not_equal(a, b, message='equal'):
-    try:
         assert a != b, message
-    except:
-        AssertionError(message)
 #print '\rassert_not_equal function tests: \n'
 #assert_not_equal(3, 5)
 #assert_not_equal(4, 4)
 
 
 def assert_true(x, message='false'):
-    try:
         assert x, message
-    except:
-        AssertionError(message)
 #print '\rassert_true function tests: \n'
 #assert_true(1)
 #assert_true(0)
 
 
 def assert_false(x, message='true'):
-    try:
         assert not x, message
-    except:
-        AssertionError(message)
 
 #print '\rassert_false function tests: \n'
 #assert_false(1)
@@ -49,10 +37,7 @@ def assert_false(x, message='true'):
 
 
 def assert_is(a, b, message=' is not'):
-    try:
         assert a is b, message
-    except:
-        AssertionError(message)
 
 #print '\rassert_is function tests: \n'
 #assert_is(3, 5)
@@ -60,10 +45,7 @@ def assert_is(a, b, message=' is not'):
 
 
 def assert_is_not(a, b, message='is'):
-    try:
         assert a is not b, message
-    except:
-        AssertionError(message)
 
 #print '\rassert_is_not function tests: \n'
 #assert_is_not(3, 5)
@@ -71,10 +53,7 @@ def assert_is_not(a, b, message='is'):
 
 
 def assert_is_none(x, message='not None'):
-    try:
         assert x is None, message
-    except:
-        AssertionError(message)
 
 #print '\rassert_is_none function tests: \n'
 #assert_is_none(None)
@@ -82,20 +61,14 @@ def assert_is_none(x, message='not None'):
 
 
 def assert_is_not_none(x, message='None'):
-    try:
         assert x is not None, message
-    except:
-        AssertionError(message)
 #print '\rassert_is_not_none function tests: \n'
 #assert_is_not_none(None)
 #assert_is_not_none(1)
 
 
 def assert_in(a, b, message='not in'):
-    try:
         assert a in b, message
-    except:
-        AssertionError(message)
 
 #print '\rassert_in function tests: \n'
 #lst1 = [1, 2, 3, 4, 5]
@@ -105,10 +78,7 @@ def assert_in(a, b, message='not in'):
 
 
 def assert_not_in(a, b, message='in'):
-    try:
         assert a not in b, message
-    except:
-        AssertionError(message)
 #print '\rassert_not_in function tests: \n'
 #lst1 = [1, 2, 3, 4, 5]
 #lst2 = [3, 6, 8, 0]
