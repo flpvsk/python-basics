dd795d9d847cfb5a8c9f387d07ee206ea5cbd9fd
yakovkoshkin <yakov.koshkin@gmail.com>
2014-04-07 11:36:02 +0400

    Currency saves rates to file and reads from it

--- yakov/currency/converter.py
+++ yakov/currency/converter.py
@@ -5,6 +5,8 @@ Created on Apr 3, 2014
 '''
 import json
 import urllib2
+from datetime import date
+import os
 
 
 URL = 'http://andreysalomatin.me/exchange-rates?from={0}&to={1}'
@@ -17,18 +19,36 @@ class currency():
         self._name = curr_tuple[0]
         self._symbol = curr_tuple[1]
         self._iso_code = curr_tuple[2]
-        self._rates = {cur[2]: self._get_rates(cur[2]) for cur in CURRENCIES}
+        self._get_rates()
 
     def __str__(self):
         return '{0} ({1})'.format(self._name, self._symbol)
 
-    def _get_rates(self, iso_code):
-        resp = urllib2.urlopen(URL.format(self._iso_code, iso_code))
-        rate_str = json.loads(resp.read())
-        return rate_str['rate']
+    def _get_rates(self):
+        d = date.today()
+        file_name = '%r-%r%r%r.json' % (self._iso_code, d.year, d.month, d.day)
+        rates = {}
+        if os.path.isfile(file_name):
+            rate_dict = self._read_rates(file_name)
+            self._rates = rate_dict
+        else:
+            for curr in CURRENCIES:
+                resp = urllib2.urlopen(URL.format(self._iso_code, curr[2]))
+                rate_dict = json.loads(resp.read())
+                rates[curr[2]] = rate_dict['rate']
+            self._save_rates(file_name, rates)
+            self._rates = rates
 
     def get_rate(self, iso_code):
         return self._rates[iso_code]
+
+    def _save_rates(self, filename, data):
+        with open(filename, 'w+') as f:
+            json.dump(data, f)
+
+    def _read_rates(self, filename):
+        with open(filename, 'r') as f:
+            return json.load(f)
 
 
 class money():
